9a18c72ed6166273a3fd4a1e4d696a68eb766f56
Denis Costa <deniscostadsc@gmail.com>
2012-03-16 12:13:07 -0300

    Adicionado path completo dad URLS nos submenus em dropdown.

--- menu/templatetags/menu.py
+++ menu/templatetags/menu.py
@@ -2,30 +2,36 @@ from django import template
 
 register = template.Library()
 
-def do_menu(parser, token):
-    try:
-        inline = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError('%r tag requires an argument.' % token.content.split()[0])
-
-    return MenuNode(inline)
-
-class MenuNode(template.Node):
-    def __init__(self, inline):
-        self.inline = inline
-
-    def _menuing(self, t):
-        menu = ''
-        for label, link in t:
-            if isinstance(link, tuple):
-                menu += '<li><a href="#">%s</a><ul>%s</ul></li>' % (label, self._menuing(link))
+def _menuing(raw_menu, old_label=''):
+    menu = ''
+    for label, link in raw_menu:
+        if isinstance(link, tuple):
+            if old_label:
+                menu += '<li><a href="%s">%s</a><ul>%s</ul></li>' %\
+                    (label.lower(), label, _menuing(link, old_label + '/' + label.lower()))
+            else:
+                menu += '<li><a href="%s">%s</a><ul>%s</ul></li>' %\
+                    (label.lower(), label, _menuing(link, label.lower()))
+        else:
+            if old_label:
+                menu += '<li><a href="%s">%s</a></li>' % (old_label + '/' + link, label)
             else:
                 menu += '<li><a href="%s">%s</a></li>' % (link, label)
+    return menu
 
-        return menu
+def do_menu(parser, token):
+    return MenuNode()
 
+def do_submenu(parser, token):
+    return SubmenuNode()
 
+class MenuNode(template.Node):
     def render(self, context):
-        return self._menuing(context['menu'])
+        return _menuing(context['menu'])
+
+class SubmenuNode(template.Node):
+    def render(self, context):
+        return _menuing(context['submenu'], )
 
 register.tag('menu', do_menu)
+register.tag('submenu', do_submenu)
