10ade52f2048d7d4488960f13ff0fd9411ea0ddc
Denis Costa <deniscostadsc@gmail.com>
2012-06-14 09:18:56 -0300

    Arruma bug na templatetag dos menus.

--- menu/templatetags/menu.py
+++ menu/templatetags/menu.py
@@ -6,6 +6,7 @@ register = template.Library()
 def _menuing(raw_menu, old_link=''):
     menu = ''
 
+    print raw_menu
     for uri, label, sub, active in raw_menu:
 
         if active:
@@ -13,23 +14,27 @@ def _menuing(raw_menu, old_link=''):
         else:
             act = ''
 
-        if isinstance(sub, tuple):
+        if isinstance(sub, list):
             # is submenu, so, I use label in link field
-            old_link = '%s/%s' % (old_link, label.lower())
-            menu += '<li%s><a href="%s">%s</a><ul>%s</ul></li>' % (act, old_link, label, _menuing(uri, old_link))
+            old_link = '%s/%s' % (old_link, uri.lower())
+            menu += '<li%s><a href="%s">%s</a><ul>%s</ul></li>' % (act, old_link, label, _menuing(sub, old_link))
         else:
             menu += '<li%s><a href="%s">%s</a></li>' % (act, '%s/%s' % (old_link, uri), label)
     return menu
 
+
 def do_menu(parser, token):
     return MenuNode()
+
 
 def do_submenu(parser, token):
     return SubmenuNode()
 
+
 class MenuNode(template.Node):
     def render(self, context):
         return _menuing(context['menu'])
+
 
 class SubmenuNode(template.Node):
     def render(self, context):
